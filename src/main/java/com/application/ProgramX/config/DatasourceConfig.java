45ed86bb7df9acc718f0d1c4d2dfb196fc3b7c2b
seif7666 <seifeldin7666@gmail.com>
2024-05-18 17:41:34 +0300

    Initial Commit

---
+++ src/main/java/com/application/ProgramX/config/DatasourceConfig.java
@@ -0,0 +1,29 @@
+package com.application.ProgramX.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@ConfigurationProperties(prefix = "datasource")
+public class DatasourceConfig {
+    @Autowired
+    Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("datasource.driverClassName")));
+        dataSource.setUrl(env.getProperty("datasource.url"));
+        dataSource.setUsername(env.getProperty("datasource.user"));
+        dataSource.setPassword(env.getProperty("datasource.password"));
+        return dataSource;
+    }
+
+}
